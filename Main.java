ec988c131216fcc939a8f54fe396355dc0f1b390
Brenda MR <brendeshyon@gmail.com>
2025-11-27 16:34:48 -0600

    Realicé el metodo printFullName

---
+++ Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	Student student = new Student("Brenda", "Montaño");
+	student.printFullName();
+		
+
+	}
+
+}
--- Student.java
+++ Student.java
@@ -1,9 +1,31 @@
 
 public class Student {
-	       String firstName;
-	       String lastName;
-	       int registration;
-	       int grade;
-	       int year;
-	   }
+	public String firstName;
+	public String lastName;
+	public int registration;
+	public int grade;
+	public int year;
+	
+	public Student(String firstName, String lastName) {
+		this.firstName = firstName.toUpperCase();
+		this.lastName = lastName.toUpperCase();
+	}
+	
+	public void printFullName() {
+		// TODO implement
+		System.out.println(this.firstName + " " + this.lastName);
+	}
 
+	public void isApproved() {
+		// TODO implement: should return true if grade >= 60
+	}
+
+	public int changeYearIfApproved() {
+		// TODO implement: the student should advance to the next year if he/she grade
+		// is >= 60
+		// Make year = year + 1, and print "Congragulations" if the student has been
+		// approved
+		return 0;
+	}
+	
+}
